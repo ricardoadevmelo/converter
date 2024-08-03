ed12b7c8e5c73356dd6462de0fdfe149bc2148ff
Ricardo Melo <ricardoadevmelo@gmail.com>
2024-08-03 11:51:08 +0100

    created the logic of the converter class

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -2,5 +2,12 @@ package exercicios.clasAtrMet.converter;
 
 public class CurrencyConverter {
 
+    public static final double IOF = 0.06;
 
+    public static double dolar;
+    public static double quantidade;
+
+    public static double converter(){
+        return (quantidade * dolar * IOF) + (quantidade * dolar);
+    }
 }

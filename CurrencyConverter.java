7ac8e2e49f58c5b907786f4760ccd102c3a653c3
Ricardo Melo <ricardoadevmelo@gmail.com>
2024-08-03 11:59:25 +0100

    program completed

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -2,7 +2,7 @@ package exercicios.clasAtrMet.converter;
 
 public class CurrencyConverter {
 
-    public static final double IOF = 0.06;
+    public static double IOF = 0.06;
 
     public static double dolar;
     public static double quantidade;
--- ProgramCurrencyConverter.java
+++ ProgramCurrencyConverter.java
@@ -14,7 +14,7 @@ public class ProgramCurrencyConverter {
         exercicios.clasAtrMet.conversao.CurrencyConverter.dolar = scanner.nextDouble();
         System.out.print("How many dollars will be purchased? ");
         exercicios.clasAtrMet.conversao.CurrencyConverter.quantidade = scanner.nextDouble();
-        System.out.print("Amount to be paid in reais = " + (CurrencyConverter.converter()));
+        System.out.print("Amount to be paid in reais = " + String.format("%.2f%n", CurrencyConverter.converter()));
 
         scanner.close();
     }

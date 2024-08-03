0ed29b6a7ed7bee5d65969d9c377cd27aeee0288
Ricardo Melo <ricardoadevmelo@gmail.com>
2024-08-03 11:56:12 +0100

    program development

--- ProgramCurrencyConverter.java
+++ ProgramCurrencyConverter.java
@@ -1,5 +1,7 @@
 package exercicios.clasAtrMet.converter;
 
+import exercicios.clasAtrMet.conversao.CurrencyConverter;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,7 +10,11 @@ public class ProgramCurrencyConverter {
         Locale.setDefault(Locale.UK);
         Scanner scanner = new Scanner(System.in);
 
-
+        System.out.print("What is the value of the dollar? ");
+        exercicios.clasAtrMet.conversao.CurrencyConverter.dolar = scanner.nextDouble();
+        System.out.print("How many dollars will be purchased? ");
+        exercicios.clasAtrMet.conversao.CurrencyConverter.quantidade = scanner.nextDouble();
+        System.out.print("Amount to be paid in reais = " + (CurrencyConverter.converter()));
 
         scanner.close();
     }

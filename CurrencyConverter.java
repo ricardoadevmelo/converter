1cdfee6e536c2d33b2eaac4db85885936f241de8
Ricardo Melo <ricardoadevmelo@gmail.com>
2024-08-03 11:42:56 +0100

    created class and program

---
+++ CurrencyConverter.java
@@ -0,0 +1,6 @@
+package exercicios.clasAtrMet.converter;
+
+public class CurrencyConverter {
+
+
+}
---
+++ ProgramCurrencyConverter.java
@@ -0,0 +1,15 @@
+package exercicios.clasAtrMet.converter;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramCurrencyConverter {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.UK);
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        scanner.close();
+    }
+}
